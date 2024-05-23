ee1abc65cd6a5b3ddc231e9d0271b9625b2e65da
elsa.m <manangelsa07@gmail.com>
2024-05-23 16:21:19 +0200

    category dto

---
+++ StoreManagement.App/DTOs/NewProductCategory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreManagement.App.DTOs
+{
+#nullable disable
+    public class NewProductCategory
+    {
+        public string Name { get; set; }
+    }
+}
