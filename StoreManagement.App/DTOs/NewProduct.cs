96cd5d2ca7a79005d4e3d56e0c490360502968c2
elsa.m <manangelsa07@gmail.com>
2024-05-23 16:21:05 +0200

    product dto

---
+++ StoreManagement.App/DTOs/NewProduct.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreManagement.App.DTOs
+{
+#nullable disable
+    public class NewProduct
+    {
+        public string Label { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
