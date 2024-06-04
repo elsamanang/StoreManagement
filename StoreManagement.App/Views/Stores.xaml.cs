d1e5d4993d4a26a7c30c98eda5f610d6defc24b8
elsa.m <manangelsa07@gmail.com>
2024-06-04 10:13:24 +0200

    new views

---
+++ StoreManagement.App/Views/Stores.xaml.cs
@@ -0,0 +1,30 @@
+﻿using StoreManagement.App.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StoreManagement.App.Views
+{
+    /// <summary>
+    /// Logique d'interaction pour Stores.xaml
+    /// </summary>
+    public partial class Stores : UserControl
+    {
+        public Stores()
+        {
+            InitializeComponent();
+            DataContext = new StoresViewModel();
+        }
+    }
+}
